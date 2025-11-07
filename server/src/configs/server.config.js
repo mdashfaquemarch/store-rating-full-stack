46072b129c03133387304d2755adc7a2dfe54da3
mdashfaquemarch <kashfaque557@gmail.com>
2025-11-08 02:58:14 +0530

    fix: Update CORS configuration to include FRONTEND environment variable

--- server/src/configs/server.config.js
+++ server/src/configs/server.config.js
@@ -11,6 +11,7 @@ const _config = {
     ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY,
     REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
     REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY,
+    FRONTEND: process.env.FRONTEND,
 }
 
 export const Config = Object.freeze(_config);
--- server/src/index.js
+++ server/src/index.js
@@ -4,29 +4,34 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import apiRoutes from './routes/index.js';
 import { errorHandler } from './middlewares/globalError.middleware.js';
+import dotenv from 'dotenv';
 
 const app = express();
 const PORT = Config.PORT;
 
-// ✅ Correct CORS setup
+dotenv.config({
+  path: "./.env"
+})
+
+
 app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    // add production domains here later
+    process.env.FRONTEND
   ],
-  credentials: true, // 👈 lowercase 'c'
+  credentials: true, 
 }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// ✅ API routes
+//  api routes
 app.use("/api", apiRoutes);
 
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(` Server running at http://localhost:${PORT}`);
 });
