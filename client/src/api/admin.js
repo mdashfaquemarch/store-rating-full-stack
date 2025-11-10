1db7f13ba0de384271bc5d1d9403a42c6316fc0b
mdashfaquemarch <kashfaque557@gmail.com>
2025-11-10 13:36:37 +0530

    fix: Update API_BASE URL to include version in multiple API files

--- client/src/api/admin.js
+++ client/src/api/admin.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "https://store-rating-backend-xuoa.onrender.com";
+const API_BASE = "https://store-rating-backend-xuoa.onrender.com/api/v1";
 
 const api = axios.create({
   baseURL: API_BASE,
